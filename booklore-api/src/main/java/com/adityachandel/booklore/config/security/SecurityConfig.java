41891cf71a18bb2a86406cefb240b4f3ded4f23b
aditya.chandel <>
2025-03-12 11:44:15 -0600

    Fix cors

--- booklore-api/src/main/java/com/adityachandel/booklore/config/security/SecurityConfig.java
+++ booklore-api/src/main/java/com/adityachandel/booklore/config/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setExposedHeaders(List.of("Content-Disposition"));
