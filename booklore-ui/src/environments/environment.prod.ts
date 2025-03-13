a63b6b78956e6f7f6667f09f301449ff47c8f93a
aditya.chandel <>
2025-03-13 14:18:02 -0600

    Fix url

--- booklore-ui/src/environments/environment.prod.ts
+++ booklore-ui/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_CONFIG: {
-    BASE_URL: `http://localhost:${window.location.port}`,
-    BROKER_URL: `ws://localhost:${window.location.port}/ws`,
+    BASE_URL: `http://${window.location.hostname}:${window.location.port}`,
+    BROKER_URL: `ws://${window.location.hostname}:${window.location.port}/ws`,
   },
 };
