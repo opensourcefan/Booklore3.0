8ceb4132f754d8c552ef001d6779c009ce813d8b
mswolfer <42715053+mswolfer@users.noreply.github.com>
2025-03-16 13:52:52 -0500

    Fixed BASE_URL protocol (for proxy support) in environment.prod.ts

--- booklore-ui/src/environments/environment.prod.ts
+++ booklore-ui/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_CONFIG: {
-    BASE_URL: `http://${window.location.hostname}:${window.location.port}`,
+    BASE_URL: `${window.location.protocol}//${window.location.hostname}:${window.location.port}`,
     BROKER_URL: `ws://${window.location.hostname}:${window.location.port}/ws`,
   },
 };
