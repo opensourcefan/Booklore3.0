d08f03b17e4882fcdc02ba37313c113328b2a520
opensourcefan <opensourceforme@gmail.com>
2026-03-31 14:34:26 -0700

    fix: remove unused Optional import

--- booklore-ui/src/app/features/metadata/component/book-metadata-center/book-metadata-center.component.ts
+++ booklore-ui/src/app/features/metadata/component/book-metadata-center/book-metadata-center.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnDestroy, OnInit, Optional} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../../../settings/user-management/user.service';
 import {Book, BookRecommendation} from '../../../book/model/book.model';
