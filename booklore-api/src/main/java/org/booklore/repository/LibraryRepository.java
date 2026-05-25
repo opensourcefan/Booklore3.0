8382557fc21f4ac316647c7ab39fb5b7c0b0b446
opensourcefan <opensourceforme@gmail.com>
2026-05-24 18:34:22 -0700

    fix(api): eager fetch libraryPaths using EntityGraph to prevent LazyInitializationException in directory tagging task

--- booklore-api/src/main/java/org/booklore/repository/LibraryRepository.java
+++ booklore-api/src/main/java/org/booklore/repository/LibraryRepository.java
@@ -1,6 +1,7 @@
 package org.booklore.repository;
 
 import org.booklore.model.entity.LibraryEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface LibraryRepository extends JpaRepository<LibraryEntity, Long>, JpaSpecificationExecutor<LibraryEntity> {
 
+    @EntityGraph(attributePaths = {"libraryPaths"})
     List<LibraryEntity> findByIdIn(List<Long> ids);
 }
