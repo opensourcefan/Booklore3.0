30b875c901175cbe7c0c40e9ac582396778ae010
opensourcefan <opensourceforme@gmail.com>
2026-05-26 20:12:49 -0700

    Fix icon size and SVG viewBox

--- booklore-ui/src/app/shared/service/app-config.service.ts
+++ booklore-ui/src/app/shared/service/app-config.service.ts
@@ -385,7 +385,7 @@ export class AppConfigService {
   }
 
   private setFaviconColor(color: string): void {
-    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2.5 0 19 19" fill="none">
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0.5 2.175 13 13" fill="none">
       <path d="M11.902 14.114V4.337l1.108 1.108v9.777H2.652A1.664 1.664 0 0 1 .99 13.559V4.9a1.665 1.665 0 0 1 1.662-1.663h.664V7.25l.874-.645.875.645V3.236h5.729v9.77h-7.85a.978.978 0 0 0-.626.111.56.56 0 0 0-.006.88.613.613 0 0 0 .39.117h9.2z" fill="${color}"/>
     </svg>`;
     const blob = new Blob([svg], {type: 'image/svg+xml'});
