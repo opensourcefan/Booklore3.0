8008f31f9ea36d3a7130607e7a6d69bc7f7dfb85
opensourcefan <opensourceforme@gmail.com>
2026-06-11 18:12:26 -0700

    fix(db): resolve MariaDB RENAME CONSTRAINT syntax error in V151 migration

--- fable-api/src/main/resources/db/migration/V151__Rename_Booklore_Columns.sql
+++ fable-api/src/main/resources/db/migration/V151__Rename_Booklore_Columns.sql
@@ -5,7 +5,8 @@ ALTER TABLE koreader_user RENAME COLUMN sync_with_booklore_reader TO sync_with_f
 ALTER TABLE koreader_user RENAME COLUMN booklore_user_id TO fable_user_id;
 
 -- Rename foreign key constraint fk_booklore_user to fk_fable_user
-ALTER TABLE koreader_user RENAME CONSTRAINT fk_booklore_user TO fk_fable_user;
+ALTER TABLE koreader_user DROP FOREIGN KEY fk_booklore_user;
+ALTER TABLE koreader_user ADD CONSTRAINT fk_fable_user FOREIGN KEY (fable_user_id) REFERENCES users (id);
 
 -- Rename permission_bulk_reset_booklore_read_progress to permission_bulk_reset_fable_read_progress
 ALTER TABLE user_permissions RENAME COLUMN permission_bulk_reset_booklore_read_progress TO permission_bulk_reset_fable_read_progress;
